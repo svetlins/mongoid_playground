337a24ff5cb7128ba42eac98e9d1426de54ba538
Svetlin Simonyan <svetlin.s@gmail.com>
2012-04-04 15:01:42 +0300

    Add one more spec for relational person

--- spec/models/relational_person_spec.rb
+++ spec/models/relational_person_spec.rb
@@ -12,4 +12,8 @@ describe RelationalPerson do
       RelationalPerson.new('birthday(3i)' => '31', 'birthday(2i)' => '2', 'birthday(1i)' => '2001')
     }.should_not raise_exception
   end
+
+  it 'converts an invalid birthday to valid through wrapping it' do
+    RelationalPerson.new('birthday(3i)' => '31', 'birthday(2i)' => '2', 'birthday(1i)' => '2001').birthday.should == Date.civil(2001, 3, 3)
+  end
 end
